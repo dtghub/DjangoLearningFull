1216fcc0e4201a348cb830334a0c3d68e43041a3
Derek Todd <derekhtodd@yahoo.co.uk>
2022-04-02 15:38:27 +0100

    Chapter 6, completed with unit tests

--- rango/views.py
+++ rango/views.py
@@ -1,18 +1,18 @@
-from unicodedata import category
 from django.shortcuts import render
 from django.http import HttpResponse
-from matplotlib.style import context
 from rango.models import Category, Page
 
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
 
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
 
     return render(request, 'rango/index.html', context=context_dict)
 
